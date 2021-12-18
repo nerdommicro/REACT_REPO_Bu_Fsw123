890b6cf61276cd64c45bf482c11a2e2a9779c42a
michellenesbitt <mn218@nerdymail.com>
2021-12-18 09:28:37 -0500

    mn

---
+++ week3-rolldice/rolldice/src/Style.js
@@ -0,0 +1,43 @@
+function Dice(props) {
+  
+  return
+    <div className="flexContainer">  
+    <div className="dice">   
+      <div className="dice">{props.Dice}</div> 
+      <div className="dice">{props.Dice}</div>  
+    </div>
+      <div className="buttonContainer">
+          <button onClick={props.Rolldice}>
+            Roll Dice
+          </button>
+      </div>
+    </div>
+  
+  
+}
+
+
+
+
+
+
+
+
+return
+  <Dice 
+    dice1 = {dice1} dice2 = {dice2}
+  />
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
